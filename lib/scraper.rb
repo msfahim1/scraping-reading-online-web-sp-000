6b78d79e48c042d2648674a6d020cd3eb23342df
Michael Fahim <michael_fahim@yahoo.com>
2020-03-03 02:20:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,36 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".s1")
+require_relative './course.rb'
 
-puts doc.css(".s1")
+class Scraper
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
+end
